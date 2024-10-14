a64c27931124ff40ca737bd76bc9e2afbc2562d5
frbarbre <fr.barbre@gmail.com>
2024-10-14 18:29:19 +0200

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
     });
     return config;
   },
